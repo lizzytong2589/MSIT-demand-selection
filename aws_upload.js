a1d2dadcd41b3af6839b12172ffb2d5dc723452d
Elizabeth Tong <etong@princeton.edu>
2020-07-07 16:56:11 -0400

    Update aws_upload.js

--- aws_upload.js
+++ aws_upload.js
@@ -23,12 +23,15 @@ var aws_upload = function() {
     MSIT_dst_data = MSIT_dst_data.ignore('internal_node_id');
     MSIT_dst_data = MSIT_dst_data.ignore('trial_type');
     MSIT_dst_data = MSIT_dst_data.ignore('trial_index');
-    console.log(MSIT_dst_data.json())
 
     var file_name = 'ID:' + ID + '_'+ date + '_' + time + '_results';
     var filePath = 'data/' + file_name;
+    var results = {
+        MSIT_data: MSIT_dst_data.json(),
+        interaction_data: interaction_data.json(),
+    }
                 
-    let params = {Bucket:bucketName, Key:filePath,Body: {'MSIT_data': MSIT_dst_data.json(), 'interaction_Data': interaction_data.json()}};
+    let params = {Bucket:bucketName, Key:filePath, Body: results };
     s3.upload(params, function(err, data) {
         if(err){
             console.log(err,err.stack);
