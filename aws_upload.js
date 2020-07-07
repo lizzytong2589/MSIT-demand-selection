5090020f07475545498012812b9fb564bd9bc7c1
Elizabeth Tong <etong@princeton.edu>
2020-07-07 17:39:50 -0400

    Debugging for AWS

--- aws_upload.js
+++ aws_upload.js
@@ -1,4 +1,4 @@
-var aws_upload = function(filePath, results) {
+var aws_upload = function() {
     // AWS Bucket Configurations
     var bucketName = 'princeton-ncclab-msit-dst';
     AWS.config.region = 'us-east-1';
@@ -10,6 +10,27 @@ var aws_upload = function(filePath, results) {
         apiVersion: '2012-10-17',
         params: {Bucket: bucketName}
     });
+
+    //// data getting/saving
+    // add subject ID to data
+    jsPsych.data.get().addToAll({worker_ID: ID});
+    var interaction_data = jsPsych.data.getInteractionData();
+
+    // filter data by experiment phase
+    var MSIT_dst_data = jsPsych.data.get().filterCustom(function(trial){
+        return ((trial.phase =='MSIT') || (trial.phase =='demand selection'));
+    });
+    MSIT_dst_data = MSIT_dst_data.ignore('internal_node_id');
+    MSIT_dst_data = MSIT_dst_data.ignore('trial_type');
+    MSIT_dst_data = MSIT_dst_data.ignore('trial_index');
+
+    var file_name = 'ID:' + ID + '_'+ date + '_' + time + '_results';
+    var filePath = 'data/' + file_name;
+    var results = {
+        // MSIT_data: MSIT_dst_data.csv(),
+        // interaction_data: interaction_data.csv(),
+        data: 'test',
+    }
                 
     let params = {Bucket:bucketName, Key:filePath, Body: results };
     s3.upload(params, function(err, data) {
--- index.js
+++ index.js
@@ -96,50 +96,10 @@ jsPsych.init({
     },
     preload_images: instruction_images,
     on_close: function() {
-        //// data getting/saving
-        // add subject ID to data
-        jsPsych.data.get().addToAll({worker_ID: ID, experiment_finished: false});
-        var interaction_data = jsPsych.data.getInteractionData();
-
-        // filter data by experiment phase
-        var MSIT_dst_data = jsPsych.data.get().filterCustom(function(trial){
-            return ((trial.phase =='MSIT') || (trial.phase =='demand selection'));
-        });
-        MSIT_dst_data = MSIT_dst_data.ignore('internal_node_id');
-        MSIT_dst_data = MSIT_dst_data.ignore('trial_type');
-        MSIT_dst_data = MSIT_dst_data.ignore('trial_index');
-
-        var file_name = 'ID:' + ID + '_'+ date + '_' + time + '_results';
-        var filePath = 'data/' + file_name;
-        var results = {
-            MSIT_data: MSIT_dst_data.json(),
-            interaction_data: interaction_data.json(),
-        }
         aws_upload(filePath, results);
-        console.log('close')
     },
     on_finish: function() {
-        //// data getting/saving
-        // add subject ID to data
-        jsPsych.data.get().addToAll({worker_ID: ID});
-        var interaction_data = jsPsych.data.getInteractionData();
-
-        // filter data by experiment phase
-        var MSIT_dst_data = jsPsych.data.get().filterCustom(function(trial){
-            return ((trial.phase =='MSIT') || (trial.phase =='demand selection'));
-        });
-        MSIT_dst_data = MSIT_dst_data.ignore('internal_node_id');
-        MSIT_dst_data = MSIT_dst_data.ignore('trial_type');
-        MSIT_dst_data = MSIT_dst_data.ignore('trial_index');
-
-        var file_name = 'ID:' + ID + '_'+ date + '_' + time + '_results';
-        var filePath = 'data/' + file_name;
-        var results = {
-            MSIT_data: MSIT_dst_data.csv(),
-            interaction_data: interaction_data.csv(),
-        }
-        aws_upload(filePath, results);
-
+        aws_upload();
         task_done = true;
         close();
     },
